8f51ccd60f034ec3181a3144e040ac05b577ce27
Tulazzano <mattia.tiozzo@hotmail.it>
2023-10-24 23:31:46 +0200

    quasi finito

--- AlbertoAlaggioPortfolio/src/pages/home/home.component.ts
+++ AlbertoAlaggioPortfolio/src/pages/home/home.component.ts
@@ -2,10 +2,10 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { interval } from 'rxjs';
 
 export class Images {
-  public index : number;
-  public src : string;
+  public index: number;
+  public src: string;
 
-  constructor(index = 0, src = ''){
+  constructor(index = 0, src = '') {
     this.index = index;
     this.src = src;
   }
@@ -16,7 +16,7 @@ export class Images {
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit ,OnDestroy{
+export class HomeComponent implements OnInit, OnDestroy {
 
   public numberOfImages = 5;
   public currenIndex = 1;
@@ -24,38 +24,71 @@ export class HomeComponent implements OnInit ,OnDestroy{
   public int = interval(4000);
   public subscription: any;
   public isNowScrolling = true;
-  
-  public createImgArray(length : number){
-    for(let i = 0; i < length ; i++){
+
+  public createImgArray(length: number) {
+    for (let i = 0; i < length; i++) {
       this.imagesArray.push(
-        new Images(i , i + '.jpg')
+        new Images(i, i + '.JPG')
       )
     }
   }
 
-  autoSwitchImages(){
-    if(this.isNowScrolling){
+  autoSwitchImages() {
+    if (this.isNowScrolling) {
       this.stopScrollingImg();
-    }else{
+    } else {
       this.startScrollingImg();
     }
     this.isNowScrolling = !this.isNowScrolling;
   }
 
-  public stopScrollingImg(){
+  public stopScrollingImg() {
     this.subscription.unsubscribe()
   }
 
-  public startScrollingImg(){
+  public startScrollingImg() {
     this.subscription = this.int.subscribe(
       () => {
-        if(this.currenIndex == this.numberOfImages -1){
+        if (this.currenIndex == this.numberOfImages - 1) {
           this.currenIndex = 0;
-        }else{
-          this.currenIndex ++;
+        } else {
+          this.currenIndex++;
         }
       }
     )
+  }
+
+  openContacts() {
+    const contactsContainer: HTMLElement | null = document.querySelector('.contacts-container');
+    const contacts: HTMLElement | null = document.querySelector('.contacts');
+    const contactsElem: NodeListOf<HTMLElement> | null = document.querySelectorAll('.contacts li');
+    //Toggle nav
+    contacts!.classList.toggle('contacts-active');
+    contactsContainer!.classList.toggle('contacts-container-active');
+
+    //animate Links
+    contactsElem.forEach((elem, index) => {
+      if (elem.style.animation) {
+        elem.style.animation = '';
+      } else {
+        var width = document.body.clientWidth;
+        elem.style.animation = `contactFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+      }
+    })
+  };
+
+  openBio() {
+    const bioContainer: HTMLElement | null = document.querySelector('.bio-container');
+    const bio: HTMLElement | null = document.querySelector('.bio');
+    //Toggle nav
+    bio!.classList.toggle('bio-active');
+    bioContainer!.classList.toggle('bio-container-active');
+
+    if (bio!.style.animation) {
+      bio!.style.animation = '';
+    } else {
+      bio!.style.animation = `bioFade 1s ease forwards 0.5s`;
+    }
   }
 
   ngOnInit(): void {
@@ -67,8 +100,8 @@ export class HomeComponent implements OnInit ,OnDestroy{
   }
 
 
-  constructor(){
+  constructor() {
     this.createImgArray(this.numberOfImages)
   }
-  
+
 }
